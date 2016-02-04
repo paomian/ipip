26d714c9af1bb35c4161f4dbba308e799dd977e0
paomian <qtang@leancloud.rocks>
2016-02-04 18:43:11 +0800

    (Fix)

--- src/locate.rs
+++ src/locate.rs
@@ -8,16 +8,22 @@ pub fn locate(s:&str) -> String {
 
     let client = Client::new();
 
-    let mut body = String::new();
+    let mut body:Vec<u8> = Vec::new();
     let url = ["http://freeapi.ipip.net/", s].concat();
     let _ = client.get(&url)
         .header(Connection::keep_alive())
         .send().map(|mut res|{
-            println!("Response: {}", res.status);
             match res.status {
-                hyper::Ok => {res.read_to_string(&mut body);},
-                _ => body = String::from("Oh"),
+                hyper::Ok => {res.read_to_end(&mut body);},
+                //_ => body = "Oh".into_bytes(),
+                _ => body = String::from("Oh").into_bytes(),
             }
         });
-    return body;
+    let _ = match String::from_utf8(body) {
+        Ok(o) => return o,
+        Err(e) => {
+            println!("{:?}",e);
+            return String::from("Oh");
+        },
+    };
 }
